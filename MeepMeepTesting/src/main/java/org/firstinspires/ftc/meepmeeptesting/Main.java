9c2775af64a7ea827748afbd7f159715482e66cb
schsdrobotics <>
2022-11-22 15:54:53 -0500

    first iteration of meepmeep code

--- MeepMeepTesting/src/main/java/org/firstinspires/ftc/meepmeeptesting/Main.java
+++ MeepMeepTesting/src/main/java/org/firstinspires/ftc/meepmeeptesting/Main.java
@@ -1,27 +1,38 @@
 package org.firstinspires.ftc.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class Main {
+    public static final double DEG_90 = Math.toRadians(90);
+
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(600);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 13)
+                .setDimensions(15, 17)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-36, -65.5, DEG_90)) // TODO is this 72-based or 70-based?
+                                // FIXME if splines don't work use linear
+                                // Preload
+                                .forward(53.5) // 2 tiles + 5.5 inches to reach the center
+                                .turn(Math.toRadians(-45))
+                                .forward(5) // takes us to (-32.46, -8.46)
+                                // Intake
+                                .splineToLinearHeading(new Pose2d(-46, -12, 0), Math.toRadians(180))
+                                .back(10)
+                                // Delivery
+                                .forward(10)
+                                .splineToLinearHeading(new Pose2d(-32.46, -8.46, Math.toRadians(45)), Math.toRadians(90))
+                                // Parking
+                                .splineToLinearHeading(new Pose2d(-36, -12, 0), Math.toRadians(270))
+                                .strafeRight(24)
+                                // Either move forward 24, -24, or 0 from vision
                                 .build()
                 );
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -20,8 +20,8 @@ public class DriveConstants {
     /*
      * These are motor constants that should be listed online for your motors.
      */
-    public static final double TICKS_PER_REV = 1;
-    public static final double MAX_RPM = 1;
+    public static final double TICKS_PER_REV = 560;
+    public static final double MAX_RPM = 300;
 
     /*
      * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
@@ -44,7 +44,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 2; // in
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = 20; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 13; // in
 
     /*
